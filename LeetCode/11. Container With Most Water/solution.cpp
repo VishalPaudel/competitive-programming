bfafd583dc727e480e4c0705ddc92455c1f516e8
Vishal Paudel <95016059+VishalPaudel@users.noreply.github.com>
2022-06-02 08:46:29 +0530

    Tried it again

--- LeetCode/11. Container With Most Water/solution.cpp
+++ LeetCode/11. Container With Most Water/solution.cpp
@@ -28,7 +28,25 @@ public:
         
 	int current_area = return_area(height, left_wall, right_wall);
 
-	// Shrinking the view
+	// Shrinking the waters!
+	int * temp_left = left_wall;
+	int * temp_right
+	
+	while (temp_left < right_wall && temp_right > temp_left && temp_right > temp_left) {
+		if (*temp_wall > *left_wall || *temp_wall > *left_wall) {
+		
+			// Found a prospective new wall
+			int left_area = return_area(height, left_wall, temp_wall);
+			int right_area = return_area(height, temp_wall, right_wall);
+
+			if (left_area > )
+		}
+
+		else
+
+		temp_wall++;
+
+	}
 
 	return max_area;
 
@@ -36,7 +54,12 @@ public:
     
     static int return_area(vector<int>& height, int * left, int * right) {
         
-        return min(*left, *right) * (right - left);
+	/*
+	 * Returns the area between two walls
+	 * Takes input the array of heights
+	 */
+        
+	return min(*left, *right) * (right - left);
 
     }
 
@@ -44,7 +67,7 @@ public:
 
 
 int main() {
-	
+
 	std::vector <int> my_vector = { 1, 2, 3, 4, 5 };
 
 	std::cout << Solution::return_area(my_vector, &( my_vector.front() ), &( my_vector.back() )) << std::endl;
